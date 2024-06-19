e630c564ff35d16fe913e422a1a710aac68bda67
fgitonga <mfredgitonga@gmail.com>
2024-06-19 19:45:57 +0300

    refact

---
+++ class_12/exercise2/target/debug/deps/exercise2-8a164679e9df94a6.d
@@ -0,0 +1,5 @@
+/home/fgitonga/rust/learn_rust/class_12/exercise2/target/debug/deps/libexercise2-8a164679e9df94a6.rmeta: src/lib.rs
+
+/home/fgitonga/rust/learn_rust/class_12/exercise2/target/debug/deps/exercise2-8a164679e9df94a6.d: src/lib.rs
+
+src/lib.rs:
---
+++ class_12/exercise2/target/debug/deps/exercise2-e24af8c3d462bf50.d
@@ -0,0 +1,5 @@
+/home/fgitonga/rust/learn_rust/class_12/exercise2/target/debug/deps/libexercise2-e24af8c3d462bf50.rmeta: src/lib.rs
+
+/home/fgitonga/rust/learn_rust/class_12/exercise2/target/debug/deps/exercise2-e24af8c3d462bf50.d: src/lib.rs
+
+src/lib.rs:
