65cf34f54ebc9347b7e57d31d1618f855ccf1304
fgitonga <mfredgitonga@gmail.com>
2024-07-13 01:42:18 +0300

    class 14: databases

---
+++ class_14/target/debug/deps/class_14-be82ff64c45a389b.d
@@ -0,0 +1,5 @@
+/home/fgitonga/fred/rust/learn_rust/class_14/target/debug/deps/libclass_14-be82ff64c45a389b.rmeta: src/main.rs
+
+/home/fgitonga/fred/rust/learn_rust/class_14/target/debug/deps/class_14-be82ff64c45a389b.d: src/main.rs
+
+src/main.rs:
---
+++ class_14/target/debug/deps/class_14-fb3ae4ff7b26da65.d
@@ -0,0 +1,5 @@
+/home/fgitonga/fred/rust/learn_rust/class_14/target/debug/deps/libclass_14-fb3ae4ff7b26da65.rmeta: src/main.rs
+
+/home/fgitonga/fred/rust/learn_rust/class_14/target/debug/deps/class_14-fb3ae4ff7b26da65.d: src/main.rs
+
+src/main.rs:
